485a3eee7fb24d9e579486d8325ba27b3d711c77
SungBeom <psb_94@naver.com>
2019-01-27 21:58:24 +0900

    Fit canvas pivot

---
+++ FarmClicker/Assets/ImportedAssets/2D Pixel Vegetables Bundle/readme.txt
@@ -0,0 +1,16 @@
+2D Pixel Vegetable Bundle
+By: Gabriel V. Laforge
+11/08/2016
+
+I hope this set of vegetable sprites make a welcome addition to your game. There are 41 different types 
+of vegetables, each with sliced or opened versions, and some with alternate colours, for a total of 262 sprites 
+(131 regular + 131 highlighted in black).
+
+Art assets included are:
+1. .PSD file reglar version of vegetables, with Magic Pink background included (just turn on the layer in Photoshop).
+2. .PSD file all vegetables highlighted in black, with Magic Pink background included (just turn on the layer in Photoshop).
+3. Spritesheets in .PNG format with transparent background, already sliced and named, ready for use.
+
+Thank you for your purchase, and please leave feedback in the review section!
+
+-Gab
---
+++ FarmClicker/Assets/Scripts/GrowPlant.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrowPlant : MonoBehaviour
+{
+    Text test;
+    Image testImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClick);
+        test = GetComponentInChildren<Text>();
+        testImage = transform.parent.Find("Button1").Find("Image").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ButtonClick()
+    {
+        StartCoroutine("Growing");
+    }
+
+    IEnumerator Growing()
+    {
+        test.text = "Grow\n";
+        for (int i = 1; i <= 5; i++)
+        {
+            test.text += ".";
+            if (i == 1) testImage.sprite = Resources.Load<Sprite>("Test1");//
+            else if (i == 3) testImage.sprite = Resources.Load<Sprite>("Test2");//
+            else if (i == 5) testImage.sprite = Resources.Load<Sprite>("Test3");//
+            yield return new WaitForSeconds(1.0f);
+        }
+        testImage.sprite = Resources.Load<Sprite>("Test");
+        test.text = "Test";
+    }
+}
